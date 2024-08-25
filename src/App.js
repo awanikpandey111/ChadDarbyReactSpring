0e1a9fb746d8d78243b7be1552d2000c228a552e
awanikpandey111 <awanikpandey111@gmail.com>
2024-08-26 00:45:03 +0530

    ch 25 regarding adding delete fun

--- src/App.js
+++ src/App.js
@@ -31,7 +31,13 @@ function App() {
       setTodos(todos=>[...todos,newTodo])
       console.log(todos);
     }
-  }
+    const deleteTodo=(deleteTodoRowNumber)=>{
+        let filtered=todos.filter(function(value){
+          return value.rowNumber!=deleteTodoRowNumber
+        })
+        setTodos(filtered);
+    }
+  
   return (
     <div className='mt-5 container'>
       <div className='card'>
@@ -39,8 +45,8 @@ function App() {
             Your todos
         </div>
         <div className='card-body'>
-          <TodoTable todos={todos}/>
-          <button className='btn btn-primary' onClick={addTodo}>Add New Todo</button>
+          <TodoTable todos={todos} deleteTodo={deleteTodo}/>
+          <button className='btn btn-primary' >Add New Todo</button>
           <NewTodoForm addTodo={addTodo}/>
         </div>
         </div>
