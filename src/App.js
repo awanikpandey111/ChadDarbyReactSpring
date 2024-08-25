8d20a8ff3a0c480d892a1cb68356f3640c905a13
awanikpandey111 <awanikpandey111@gmail.com>
2024-08-26 00:01:43 +0530

    First commit after adding add functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import TodoRowItem from './components/TodoRowItem';
+import TodoTable from './components/TodoTable.js'
+import { Component, useState } from 'react';
+import NewTodoForm from './components/NewTodoForm.js';
 
 function App() {
+
+  const [todos,setTodos]=useState([
+    {rowNumber:1, rowDescription:'Feed Puppy', rowAssigned:'User One'},
+    {rowNumber:2, rowDescription:"Water Plants", rowAssigned:'User Two'},
+    {rowNumber:3, rowDescription:'Make Dinner', rowAssigned:'User Three'},
+    {rowNumber:4, rowDescription:'Make Lunch', rowAssigned:'User four'}
+  ])
+
+  const addTodo=(description,assigned)=>{
+    let rowNumber=0;
+    if(todos.length > 0){
+      rowNumber=todos[todos.length-1].rowNumber+1;
+    }else{
+      rowNumber=1;
+    }
+      const newTodo={
+        rowNumber:rowNumber,
+        rowDescription: description,
+        rowAssigned: assigned
+      };
+      // todos.push(newTodo);this will not rerender
+      setTodos(todos=>[...todos,newTodo])
+      console.log(todos);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header'>
+            Your todos
+        </div>
+        <div className='card-body'>
+          <TodoTable todos={todos}/>
+          <button className='btn btn-primary' onClick={addTodo}>Add New Todo</button>
+          <NewTodoForm addTodo={addTodo}/>
+        </div>
+        </div>
     </div>
   );
 }
